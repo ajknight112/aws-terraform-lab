d2bcff6e82713d856e5f80cedacf1fd431dd8c71
ajknight112 <amadhava@emich.edu>
2025-12-01 17:40:03 -0500

    Convert submodule to folder and add Terraform code

---
+++ aws-terraform-lab/main.tf
@@ -0,0 +1,77 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl enable httpd
+    systemctl start httpd
+
+    cat << 'HTML' > /var/www/html/index.html
+    <html>
+      <body>
+        <h1>Terraform Web Server Version 2</h1>
+        <p>This is updated with IAC<p>
+      </body>
+    </html>
+    HTML
+  EOF
+
+  tags = {
+    Name = "Terraform-WebServer"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
+
